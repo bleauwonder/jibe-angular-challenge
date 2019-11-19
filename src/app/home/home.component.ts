17e258145889244eb6f798e07f707f62b82459c2
bleauwonder <elyzableau.om@gmail.com>
2019-11-18 21:45:58 -0500

    adjusted dev title

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -9,7 +9,7 @@ import { ApiService } from '../services/api.service';
 export class HomeComponent implements OnInit {
 
   jobs: Object;
-
+  panelOpenState = false;
   constructor(private data: ApiService) { }
 
   ngOnInit() {
